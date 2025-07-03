f61579fe7027524a022b47cb33e5a797663149d7
Kanash Andrey <sp3cia1sit@gmail.com>
2025-07-03 13:54:33 +0300

    Fixed errors with the display of forms

---
+++ Application/Migrations/20250703104558_FixedBugs.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedBugs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_Forms_FormId",
+                table: "Questions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Questions_FormId",
+                table: "Questions");
+
+            migrationBuilder.AddColumn<int>(
+                name: "FormViewModelId",
+                table: "Questions",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_FormViewModelId",
+                table: "Questions",
+                column: "FormViewModelId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_Forms_FormViewModelId",
+                table: "Questions",
+                column: "FormViewModelId",
+                principalTable: "Forms",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_Forms_FormViewModelId",
+                table: "Questions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Questions_FormViewModelId",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "FormViewModelId",
+                table: "Questions");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_FormId",
+                table: "Questions",
+                column: "FormId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_Forms_FormId",
+                table: "Questions",
+                column: "FormId",
+                principalTable: "Forms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
