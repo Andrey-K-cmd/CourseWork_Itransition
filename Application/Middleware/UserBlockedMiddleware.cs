82aa5beaf10b9fd3b2c8c45ca57d5464ba910498
Kanash Andrey <sp3cia1sit@gmail.com>
2025-07-06 21:46:36 +0300

    Improved user blocking

---
+++ Application/Middleware/UserBlockedMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Application.Models.User;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace Application.Middleware
+{
+    public class UserBlockedMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public UserBlockedMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext, UserManager<UserModel> userManager, SignInManager<UserModel> signInManager)
+        {
+            if (httpContext.User.Identity?.IsAuthenticated == true)
+            {
+                var id = httpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                var user = await userManager.FindByIdAsync(id);
+
+                if (user != null && user.IsBlocked)
+                {
+                    await signInManager.SignOutAsync();
+                    httpContext.Response.Redirect("/Account/Login");
+                    return;
+                }
+            }
+
+            await _next(httpContext);
+        }
+    }
+}
--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e898054cd084ba9b191983848682e41c94d16ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4b060b1961c2909c50875e58bd97d796ba08325")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
